d113d257cecf1bc86176aab5a885d12da2e63086
Piush-hexaware <piushs.hexaware@gmail.com>
2019-01-25 23:39:31 +0530

    s

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ restService.post("/echo", function(req, res) {
     req.body.result &&
     req.body.result.parameters &&
     req.body.result.parameters.first_number
-      ? req.body.result.parameters.first_number + req.body.result.parameters.second_number
+      ? req.body.result.parameters.second_number + 1
       : "Seems like some problem. Speak again.";
 
   return res.json({
