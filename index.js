b24861baa8cb8c5d3ad83d51095cfe2144b477f1
Piush-hexaware <piushs.hexaware@gmail.com>
2019-01-26 00:16:54 +0530

    s

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ restService.post("/echo", function(req, res) {
     req.body.result &&
     req.body.result.parameters &&
     req.body.result.parameters.first_number
-      ? req.body.result.parameters.second_number + req.body.result.parameters.first_number
+      ? parseInt(req.body.result.parameters.second_number )+ parseInt(req.body.result.parameters.first_number)
       : "Seems like some problem from addition. Speak again.";
   }
 
