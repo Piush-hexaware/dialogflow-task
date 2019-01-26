ce9fb478fcddd95c25770255622233b07c60dcd6
Piush-hexaware <piushs.hexaware@gmail.com>
2019-01-27 00:42:19 +0530

    s

--- index.js
+++ index.js
@@ -12,34 +12,59 @@ restService.use(
 );
 restService.use(bodyParser.json());
 var result;
-restService.post("/echo", function(req, res) {
-  
-  if (req.body.result.parameters.addition == "add"){
-    result =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.first_number
-      ? parseInt(req.body.result.parameters.second_number )+ parseInt(req.body.result.parameters.first_number)
-      : "Seems like some problem from addition. Speak again.";
-  }
 
-  else if(req.body.result.parameters.subtraction == "sub")
-  {
-    result =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.first_number
-      ? req.body.result.parameters.second_number - req.body.result.parameters.first_number
-      : "Seems like some problem for subtraction. Speak again.";
-  }
+restService.post("/cal",function(req,res){
 
+console.log("received a post request");
+if(!req.body) return res.sendStatus(400);
+res.setHeader('Content-Type','application/json');
+console.log("here is the post request from dialogflow");
+console.log(req.body);
+console.log("parameter form dilaogflow" + req.body.queryResult.parameters['first_number']);
+let response =" ";
+let w ="my name is piyush";
+let responseObj={
+ 
+  "fulfillmentText" : response
+  ,"fulfillmentMessages":[{"text": { "text": [w] }} ]
+  ,"source":"" 
+}
+console.log("response data " + responseObj);
+return res.json(responseObj);
 
-  return res.json({
-    speech: result,
-    displayText: result,
-    source: "calc-app"
-  });
 });
+
+
+
+// restService.post("/cal", function(req, res) {
+  
+//   if (req.body.result.parameters.addition == "add"){
+//     result =
+//     req.body.result &&
+//     req.body.result.parameters &&
+//     req.body.result.parameters.first_number
+//       ? parseInt(req.body.result.parameters.second_number )+ parseInt(req.body.result.parameters.first_number)
+//       : "Seems like some problem from addition. Speak again.";
+//   }
+
+//   else if(req.body.result.parameters.subtraction == "sub")
+//   {
+//     result =
+//     req.body.result &&
+//     req.body.result.parameters &&
+//     req.body.result.parameters.first_number
+//       ? req.body.result.parameters.second_number - req.body.result.parameters.first_number
+//       : "Seems like some problem for subtraction. Speak again.";
+//   }
+
+
+//   return res.json({
+//     speech: result,
+//     displayText: result,
+//     source: "calc-app"
+//   });
+// });
+
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
 });
