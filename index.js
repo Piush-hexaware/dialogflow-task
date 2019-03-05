d6cb9d9a3f51774f320daee14500937280048e44
Piush Singh <43833@hexaware.com>
2019-03-05 16:27:57 +0530

    s

--- index.js
+++ index.js
@@ -153,53 +153,56 @@ restService.post("/cal",function(req,res){
   
     result = second_number + first_number ;
     
-    responseObj={
+    responseObj= {
       "payload": {
         "google": {
           "expectUserResponse": true,
           "richResponse": {
             "items": [
               {
-                "simpleResponse": {
-                  "textToSpeech": "Choose a item"
+                "carouselBrowse": {
+                  "items": [
+                    {
+                      "title": "Title of item 1",
+                      "openUrlAction": {
+                        "url": ""
+                      },
+                      "description": "Description of item 1",
+                      "footer": "Item 1 footer",
+                      "image": {
+                        "url": "",
+                        "accessibilityText": "Google Assistant Bubbles"
+                      }
+                    },
+                    {
+                      "title": "Title of item 2",
+                      "openUrlAction": {
+                        "url": "https://google.com"
+                      },
+                      "description": "Description of item 2",
+                      "footer": "Item 2 footer",
+                      "image": {
+                        "url": "https://developers.google.com/actions/assistant.png",
+                        "accessibilityText": "Google Assistant Bubbles"
+                      }
+                    }
+                  ]
                 }
               }
             ]
           },
-          "systemIntent": {
-            "intent": "actions.intent.OPTION",
-            "data": {
-              "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
-              "carouselSelect": {
-                "items": [
-                  {
-                    "optionInfo": {
-                      "key": "first title"
-                    },
-                    "description": "**Poetic Language** \n 4 units \n Graded - A",
-                    "image": {
-                      "url": "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
-                      "accessibilityText": "first alt"
-                    },
-                    "title": "first title"
-                  },
-                  {
-                    "optionInfo": {
-                      "key": "second"
-                    },
-                    "description": "second description",
-                    "image": {
-                      "url": "https://lh3.googleusercontent.com/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw",
-                      "accessibilityText": "second alt"
-                    },
-                    "title": "second title"
-                  }
-                ]
-              }
-            }
+          "userStorage": "{\"data\":{}}"
+        }
+      },
+      "outputContexts": [
+        {
+          "name": "projects/temperatureconvertersample/agent/sessions/518488a5-09f6-4a36-8950-942f595b70b8/contexts/_actions_on_google",
+          "lifespanCount": 99,
+          "parameters": {
+            "data": "{}"
           }
         }
-      }
+      ]
     }
    
   
@@ -265,3 +268,8 @@ restService.post("/cal",function(req,res){
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
 });
+
+
+
+
+
