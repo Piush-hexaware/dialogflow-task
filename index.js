eb2368776520a5d4f92a727778bdfc1c16112eb8
Piush-hexaware <46959519+Piush-hexaware@users.noreply.github.com>
2019-05-21 12:20:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ restService.use(bodyParser.json());
 
 restService.get("/authorize",function(req,res){
 console.log("authorize call",JSON.stringify(req.query))
+  let url="https://dev-azxluogv.auth0.com/login?state=g6Fo2SBqM1hBS0UzY0V1TlB4anVjQ0p5Nmw1QjRfLWpmZlVucaN0aWTZIEllR1ptd1l6WmlBNjBvRXdndXJMcE0yQW1qM0pvYnhUo2NpZNkgajJpUklfTmpLd2Q3RXpRZFo0YWtTTWwyb1RXamNvQkw&client=j2iRI_NjKwd7EzQdZ4akSMl2oTWjcoBL&protocol=oauth2&prompt=consent&response_type=code&redirect_uri=https%3A%2F%2Foauth-redirect.googleusercontent.com%2Fr%2Flocate-2dd42&scope=openid%20profile%20email%20offline_access"
+  res.redirect(307,url);
   });
   
 
