93e2566f19e298043dea66e2d940f3a6b2dd366a
Piush-hexaware <46959519+Piush-hexaware@users.noreply.github.com>
2019-05-21 15:17:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ restService.use(bodyParser.json());
 
 restService.get("/authorize",function(req,res){
 console.log("authorize call",JSON.stringify(req.query))
-  let url="https://oegesbot.auth0.com/login?state=g6Fo2SA0ZHBYNEtOV21tc3NlWnBfMF9JeUxubzg0WWVUTlRWUaN0aWTZIGVyQlptQnJmb3hkUllTLS1UeUdjV3AyUUVDZTlwNEswo2NpZNkgMmVCcVd0bFM5OUswNjdNUFg0OXluN0tJOFQ5NGhvTV8&client=2eBqWtlS99K067MPX49yn7KI8T94hoM_&protocol=oauth2&prompt=consent&response_type=code&redirect_uri=https%3A%2F%2Foauth-redirect.googleusercontent.com%2Fr%2Foeg-es-campus-admin&scope=openid%20profile%20email%20offline_access&connection=OegEsBotNew"
+  let url="https://oegesbot.auth0.com/authorize?client_id=2eBqWtlS99K067MPX49yn7KI8T94hoM_&response_type=code&redirect_uri=https://oauth-redirect.googleusercontent.com/r/oeg-es-campus-admin&prompt=consent&scope=openid%20profile%20email%20offline_access&state=g6Fo2SA0ZHBYNEtOV21tc3NlWnBfMF9JeUxubzg0WWVUTlRWUaN0aWTZIGVyQlptQnJmb3hkUllTLS1UeUdjV3AyUUVDZTlwNEswo2NpZNkgMmVCcVd0bFM5OUswNjdNUFg0OXluN0tJOFQ5NGhvTV8&client=2eBqWtlS99K067MPX49yn7KI8T94hoM_&connection=OegEsBotNew&audience=OegEsBot"
   res.redirect(307,url);
   });
   
