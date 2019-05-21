d487918fc3c402b7a8c24ba5059fb04c89f57164
Piush-hexaware <46959519+Piush-hexaware@users.noreply.github.com>
2019-05-21 11:56:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ restService.use(bodyParser.json());
 
 
 restService.get("/authorize",function(req,res){
-console.log("authorize call",JSON.stringify(req.body))
+console.log("authorize call",JSON.stringify(req.body.client_id))
   });
   
 
