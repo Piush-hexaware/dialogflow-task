f0cc911a2411a5377e278ce6c2b885ce325bc05e
Piush-hexaware <46959519+Piush-hexaware@users.noreply.github.com>
2019-05-21 13:10:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ restService.use(bodyParser.json());
 
 restService.get("/authorize",function(req,res){
 console.log("authorize call",JSON.stringify(req.query))
-  let url="https://dev-azxluogv.auth0.com/login?state=g6Fo2SBqM1hBS0UzY0V1TlB4anVjQ0p5Nmw1QjRfLWpmZlVucaN0aWTZIEllR1ptd1l6WmlBNjBvRXdndXJMcE0yQW1qM0pvYnhUo2NpZNkgajJpUklfTmpLd2Q3RXpRZFo0YWtTTWwyb1RXamNvQkw&client=j2iRI_NjKwd7EzQdZ4akSMl2oTWjcoBL&protocol=oauth2&prompt=consent&response_type=code&redirect_uri=https%3A%2F%2Foauth-redirect.googleusercontent.com%2Fr%2Flocate-2dd42&scope=openid%20profile%20email%20offline_access"
+  let url="https://oegesbot.auth0.com/login?state=g6Fo2SA0ZHBYNEtOV21tc3NlWnBfMF9JeUxubzg0WWVUTlRWUaN0aWTZIGVyQlptQnJmb3hkUllTLS1UeUdjV3AyUUVDZTlwNEswo2NpZNkgMmVCcVd0bFM5OUswNjdNUFg0OXluN0tJOFQ5NGhvTV8&client=2eBqWtlS99K067MPX49yn7KI8T94hoM_&protocol=oauth2&prompt=consent&response_type=code&redirect_uri=https%3A%2F%2Foauth-redirect.googleusercontent.com%2Fr%2Foeg-es-campus-admin&scope=openid%20profile%20email%20offline_access&connection=OegEsBotNew"
   res.redirect(307,url);
   });
   
