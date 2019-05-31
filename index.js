3b096431fd40dcdb0766109f4f9a3d005083459f
Piush-hexaware <46959519+Piush-hexaware@users.noreply.github.com>
2019-05-31 14:30:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,42 +10,42 @@ restService.use(
     extended: true
   })
 );
-
+var responseObj;
 restService.use(bodyParser.json());
 
 restService.post("/api",function(req,res){
 console.log("received a post request"+ JSON.stringify(req.body));
 if(!req.body) return res.sendStatus(400);
 res.setHeader('Content-Type','application/json');
-let responseObj=
-{
-  "payload": {
-    "google": {
-      "expectUserResponse": true,
-      "richResponse": {
-        "items": [
-          {
-            "simpleResponse": {
-              "textToSpeech": "Hi! Welcome to Actions on Google!"
+if(req.body.result.resolvedQuery == "GOOGLE_ASSISTANT_WELCOME") {
+  responseObj=
+  {
+    "payload": {
+      "google": {
+        "expectUserResponse": true,
+        "richResponse": {
+          "items": [
+            {
+              "simpleResponse": {
+                "textToSpeech": "Hi! Welcome to Actions on Google!"
+              }
+            },
+            {
+              "simpleResponse": {
+                "textToSpeech": "I can send you alerts. Would you like that?"
+              }
             }
-          },
-          {
-            "simpleResponse": {
-              "textToSpeech": "I can send you alerts. Would you like that?"
+          ],
+          "suggestions": [
+            {
+              "title": "Alert me"
             }
-          }
-        ],
-        "suggestions": [
-          {
-            "title": "Alert me"
-          }
-        ]
+          ]
+        }
       }
     }
   }
 }
-
-
 console.log("response data " + JSON.stringify(responseObj));
 return res.json(responseObj);
 
