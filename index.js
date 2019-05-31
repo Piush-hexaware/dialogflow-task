b86d8650a18230fd68fe4280e935a628a29286ed
Piush-hexaware <46959519+Piush-hexaware@users.noreply.github.com>
2019-05-31 15:50:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ const restService = express();
 
 
 
-{
-  "type": "service_account",
-  "project_id": "notification-c32a2",
-  "private_key_id": "80b677422d1127d1636d1fb3718239961f5c5163",
-  "private_key": "-----BEGIN PRIVATE KEY-----\n-----END PRIVATE KEY-----\n",
-  "client_email": "",
-  "client_id": "110541901143281921091",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/notifications%40notification-c32a2.iam.gserviceaccount.com"
-}
+// {
+//   "type": "service_account",
+//   "project_id": "notification-c32a2",
+//   "private_key_id": "80b677422d1127d1636d1fb3718239961f5c5163",
+//   "private_key": "-----BEGIN PRIVATE KEY-----\n-----END PRIVATE KEY-----\n",
+//   "client_email": "",
+//   "client_id": "110541901143281921091",
+//   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+//   "token_uri": "https://oauth2.googleapis.com/token",
+//   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+//   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/notifications%40notification-c32a2.iam.gserviceaccount.com"
+// }
 
 
 
