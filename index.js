6c3b9d858bd61838eeb2d6dc6133c65e15e1483f
Piush-hexaware <46959519+Piush-hexaware@users.noreply.github.com>
2019-05-31 15:00:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,44 +18,14 @@ console.log("received a post request"+ JSON.stringify(req.body));
 if(!req.body) return res.sendStatus(400);
 res.setHeader('Content-Type','application/json');
 if(req.body.queryResult.queryText == "GOOGLE_ASSISTANT_WELCOME") {
-  responseObj=
-  {
-    "payload": {
-      "google": {
-        "expectUserResponse": true,
-        "richResponse": {
-          "items": [
-            {
-              "simpleResponse": {
-                "textToSpeech": "Hi! Welcome to Actions on Google!"
-              }
-            },
-            {
-              "simpleResponse": {
-                "textToSpeech": "I can send you alerts. Would you like that?"
-              }
-            }
-          ],
-          "suggestions": [
-            {
-              "title": "Alert me"
-            }
-          ]
-        }
-      }
-    }
-  }
-}
-  else {
-  {
-  responseObj={"payload": {
+  responseObj=  {"payload": {
     "google": {
       "expectUserResponse": true,
       "systemIntent": {
         "intent": "actions.intent.PERMISSION",
         "data": {
           "@type": "type.googleapis.com/google.actions.v2.PermissionValueSpec",
-          "optContext": "To deliver your order",
+          "optContext": "Hi! Welcome to Actions on Google! \nI can send you alerts. Would you like that?",
           "permissions": [
             "NAME",
             "DEVICE_PRECISE_LOCATION"
@@ -64,8 +34,8 @@ if(req.body.queryResult.queryText == "GOOGLE_ASSISTANT_WELCOME") {
       }
     }
   }
-}}
-  }
+}
+}
 console.log("response data " + JSON.stringify(responseObj));
 return res.json(responseObj);
 
