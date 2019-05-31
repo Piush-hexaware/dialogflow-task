acfde0848cd0d2e442c503fd8ee37e465b327fa6
Piush Singh <43833@hexaware.com>
2019-05-31 14:14:25 +0530

    push notification

--- index.js
+++ index.js
@@ -13,29 +13,48 @@ restService.use(
 
 restService.use(bodyParser.json());
 
+restService.post("/api",function(req,res){
+console.log("received a post request"+ JSON.stringify(req.body));
+if(!req.body) return res.sendStatus(400);
+res.setHeader('Content-Type','application/json');
+responseObj=
+{
+  "payload": {
+    "google": {
+      "expectUserResponse": true,
+      "richResponse": {
+        "items": [
+          {
+            "simpleResponse": {
+              "textToSpeech": "Hi! Welcome to Actions on Google!"
+            }
+          },
+          {
+            "simpleResponse": {
+              "textToSpeech": "I can send you alerts. Would you like that?"
+            }
+          }
+        ],
+        "suggestions": [
+          {
+            "title": "Alert me"
+          }
+        ]
+      }
+    }
+  }
+}
 
-restService.get("/authorize",function(req,res){
-console.log("authorize call",JSON.stringify(req.query))
-  let url="https://oegesbot.auth0.com/authorize?client_id=2eBqWtlS99K067MPX49yn7KI8T94hoM_&response_type=code&redirect_uri=https://oauth-redirect.googleusercontent.com/r/oeg-es-campus-admin&prompt=consent&scope=openid%20profile%20email%20offline_access&state=g6Fo2SA0ZHBYNEtOV21tc3NlWnBfMF9JeUxubzg0WWVUTlRWUaN0aWTZIGVyQlptQnJmb3hkUllTLS1UeUdjV3AyUUVDZTlwNEswo2NpZNkgMmVCcVd0bFM5OUswNjdNUFg0OXluN0tJOFQ5NGhvTV8&client=2eBqWtlS99K067MPX49yn7KI8T94hoM_&connection=OegEsBotNew&audience=OegEsBot"
-  res.redirect(307,url);
-  });
-  
 
+console.log("response data " + JSON.stringify(responseObj));
+return res.json(responseObj);
 
-restService.post("/token",function(req,res){
-console.log("token call ",JSON.stringify(req.body)) 
-  });
-
-
-restService.post("/redirect",function(req,res){
-console.log(" redirect data call ",JSON.stringify(req.body)) 
-  });
-
-
-restService.listen(process.env.PORT || 8000, function() {
-  console.log("Server up and listening");
 });
 
+
+restService.listen(process.env.PORT || 4000, function() {
+  console.log("Server up and listening");
+});
 
 
 
