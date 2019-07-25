a345b2673357a91e819bd5e5fbdea5324882fd84
Piush-hexaware <46959519+Piush-hexaware@users.noreply.github.com>
2019-07-25 18:10:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ let responseObj= null;
           },
           {
             "basicCard": {
-              "title": "Title: this is a title",
+              "title": "Title: this is a title"+Math.random(),
               "subtitle": "This is a subtitle",
               "formattedText": "This is a basic card.  Text in a basic card can include \"quotes\" and\n        most other unicode characters including emoji 📱.  Basic cards also support\n        some markdown formatting like *emphasis* or _italics_, **strong** or\n        __bold__, and ***bold itallic*** or ___strong emphasis___ as well as other\n        things like line  \nbreaks",
               "image": {
